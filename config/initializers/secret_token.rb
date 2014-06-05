5801c4400aa052303e44003e09789ad5c76191fe
Carlos García de la Barrera <carlos.garcia@lextrend.net>
2014-06-05 17:23:52 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lokusapp::Application.config.secret_key_base = 'e34c5d44887f98909ad13495f92da060f44234c740e19f9c706d010a907c94620f7535b3034f0308c9210b540876eefa77dc0c1afa5ea3ad05280e84ab8c13c1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lokusapp::Application.config.session_store :cookie_store, key: '_lokusapp_session'
