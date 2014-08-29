0692f5e4ab658c62dfdbbb9f2c9ebd7665697757
Carlos García de la Barrera <carlosgarciadelabarrera@Thor.(none)>
2014-08-29 17:49:33 +0200

    app

--- Gemfile
+++ Gemfile
@@ -19,15 +19,17 @@ gem 'therubyracer', :platform=>:ruby
 gem "redis", "~> 3.0.1"
 gem "hiredis", "~> 0.4.5"
 
+gem 'newrelic_rpm'
+
+gem 'unicorn'
+
 
 group :development do
   gem 'better_errors'
   gem 'binding_of_caller', :platforms=>[:mri_20]
-  gem 'capistrano', '~> 3.0.1'
-  gem 'capistrano-bundler'
-  gem 'capistrano-rails', '~> 1.1.0'
-  gem 'capistrano-rails-console'
-  gem 'capistrano-rvm', '~> 0.1.1'
+  gem "capistrano", "~> 2.13.4"
+  gem 'rvm-capistrano'
+
   gem 'quiet_assets'
   gem 'rails_layout'
 end
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,40 +1,80 @@
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+require "rvm/capistrano"
+set :rvm_type, :user
+require 'bundler/capistrano'
+require './config/deploy_tasks/after_deploy'
 
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+set :normalize_asset_timestamps, false
+set :repository,  "git@github.com:cgarciabarrera/LokMe.git"
+set :user, "web"
 
-# set :deploy_to, '/var/www/my_app'
-# set :scm, :git
+set :ssh_options, { :forward_agent => true }
+set :use_sudo, false
+set :keep_releases, 5
+set :scm, :git
+set :deploy_via, :remote_cache
 
-# set :format, :pretty
-# set :log_level, :debug
-# set :pty, true
+default_run_options[:pty] = true
+#default_run_options[:shell] = '/bin/bash'
 
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-# set :keep_releases, 5
+
+begin
+  case targetname
+
+
+    when 'production' then
+      # Prompt to make really sure we want to deploy into prouction
+      puts "\n\e[0;31m   ######################################################################"
+      puts "   #\n   #       Estas seguro de querer hacer un deploy a produccion?"
+      puts "   #\n   #               Introduzca y/N + o intro para continuar\n   #"
+      puts "   ######################################################################\e[0m\n"
+      proceed = STDIN.gets[0..0] rescue nil
+      exit unless proceed == 'y' || proceed == 'Y'
+
+      #set :rvm_ruby_string, 'ruby-1.9.2-p180@fundspeople'
+      set :application, "www.lokusapp.com"
+      set :deploy_to, "/home/web/apps/#{application}"
+      role :app, "www.lokusapp.com"
+      role :web, "www.lokusapp.com"
+      role :db,  "www.lokusapp.com", :primary => true
+      set :rails_env, "production"
+      set :branch, "production"
+
+      set :bundle_without, [:development]
+
+    else
+      raise Capistrano::Error, "Your targetname is invalid, please use [pre|www]"
+  end
+rescue
+  raise Capistrano::Error, "You need to pass -S targetname=[development2 | staging2 | production ]\n\nFor example:\n   cap -S targetname=production deploy"
+end
+
+
+
+# -- Unicorn stuff
+set :unicorn_binary, "bundle exec unicorn_rails"
+set :unicorn_config, "#{current_path}/config/unicorn.rb"
+set :unicorn_pid,    "#{current_path}/tmp/pids/unicorn.pid"
 
 namespace :deploy do
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
-    end
+  task :start, :roles => :app, :except => { :no_release => true } do
+    run "cd #{current_path} && #{try_sudo} #{unicorn_binary} -c #{unicorn_config} -E #{rails_env} -D"
   end
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
+  task :stop, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} kill `cat #{unicorn_pid}`"
   end
-
-  after :finishing, 'deploy:cleanup'
-
+  task :graceful_stop, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} kill -s QUIT `cat #{unicorn_pid}`"
+  end
+  task :reload, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} kill -s USR2 `cat #{unicorn_pid}`"
+  end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    stop
+    start
+  end
 end
+
+# -- Stuff to do after deploy
+after "deploy:update_code", "deploy:symlink_config", "deploy:cleanup"#, "ts:arrancar"
+after "deploy:setup","deploy:create_dirs"
