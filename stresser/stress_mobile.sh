a057c5397ba237f882ec7b2b8fc7266d9385b5e1
Carlos García de la Barrera <carlos@Mac-Pro-de-carlos.local>
2014-10-11 01:51:07 +0200

    stress mobile

---
+++ stresser/stress_mobile.sh
@@ -0,0 +1,200 @@
+ruby script_mobile.rb 1 1 &
+sleep 5
+ruby script_mobile.rb 2 1 &
+sleep 5
+ruby script_mobile.rb 3 1 &
+sleep 5
+ruby script_mobile.rb 4 1 &
+sleep 5
+ruby script_mobile.rb 5 1 &
+sleep 5
+ruby script_mobile.rb 6 1 &
+sleep 5
+ruby script_mobile.rb 7 1 &
+sleep 5
+ruby script_mobile.rb 8 1 &
+sleep 5
+ruby script_mobile.rb 9 1 &
+sleep 5
+ruby script_mobile.rb 10 1 &
+sleep 5
+ruby script_mobile.rb 11 1 &
+sleep 5
+ruby script_mobile.rb 12 1 &
+sleep 5
+ruby script_mobile.rb 13 1 &
+sleep 5
+ruby script_mobile.rb 14 1 &
+sleep 5
+ruby script_mobile.rb 15 1 &
+sleep 5
+ruby script_mobile.rb 16 1 &
+sleep 5
+ruby script_mobile.rb 17 1 &
+sleep 5
+ruby script_mobile.rb 18 1 &
+sleep 5
+ruby script_mobile.rb 19 1 &
+sleep 5
+ruby script_mobile.rb 20 1 &
+sleep 5
+ruby script_mobile.rb 21 1 &
+sleep 5
+ruby script_mobile.rb 22 1 &
+sleep 5
+ruby script_mobile.rb 23 1 &
+sleep 5
+ruby script_mobile.rb 24 1 &
+sleep 5
+ruby script_mobile.rb 25 1 &
+sleep 5
+ruby script_mobile.rb 26 1 &
+sleep 5
+ruby script_mobile.rb 27 1 &
+sleep 5
+ruby script_mobile.rb 28 1 &
+sleep 5
+ruby script_mobile.rb 29 1 &
+sleep 5
+ruby script_mobile.rb 30 1 &
+sleep 5
+ruby script_mobile.rb 31 1 &
+sleep 5
+ruby script_mobile.rb 32 1 &
+sleep 5
+ruby script_mobile.rb 33 1 &
+sleep 5
+ruby script_mobile.rb 34 1 &
+sleep 5
+ruby script_mobile.rb 35 1 &
+sleep 5
+ruby script_mobile.rb 36 1 &
+sleep 5
+ruby script_mobile.rb 37 1 &
+sleep 5
+ruby script_mobile.rb 38 1 &
+sleep 5
+ruby script_mobile.rb 39 1 &
+sleep 5
+ruby script_mobile.rb 40 1 &
+sleep 5
+ruby script_mobile.rb 41 1 &
+sleep 5
+ruby script_mobile.rb 42 1 &
+sleep 5
+ruby script_mobile.rb 43 1 &
+sleep 5
+ruby script_mobile.rb 44 1 &
+sleep 5
+ruby script_mobile.rb 45 1 &
+sleep 5
+ruby script_mobile.rb 46 1 &
+sleep 5
+ruby script_mobile.rb 47 1 &
+sleep 5
+ruby script_mobile.rb 48 1 &
+sleep 5
+ruby script_mobile.rb 49 1 &
+sleep 5
+ruby script_mobile.rb 50 1 &
+sleep 5
+
+
+ruby script_mobile.rb 51 2 &
+sleep 5
+ruby script_mobile.rb 52 2 &
+sleep 5
+ruby script_mobile.rb 53 2 &
+sleep 5
+ruby script_mobile.rb 54 2 &
+sleep 5
+ruby script_mobile.rb 55 2 &
+sleep 5
+ruby script_mobile.rb 56 2 &
+sleep 5
+ruby script_mobile.rb 57 2 &
+sleep 5
+ruby script_mobile.rb 58 2 &
+sleep 5
+ruby script_mobile.rb 59 2 &
+sleep 5
+ruby script_mobile.rb 60 2 &
+sleep 5
+ruby script_mobile.rb 61 2 &
+sleep 5
+ruby script_mobile.rb 62 2 &
+sleep 5
+ruby script_mobile.rb 63 2 &
+sleep 5
+ruby script_mobile.rb 64 2 &
+sleep 5
+ruby script_mobile.rb 65 2 &
+sleep 5
+ruby script_mobile.rb 66 2 &
+sleep 5
+ruby script_mobile.rb 67 2 &
+sleep 5
+ruby script_mobile.rb 68 2 &
+sleep 5
+ruby script_mobile.rb 69 2 &
+sleep 5
+ruby script_mobile.rb 70 2 &
+sleep 5
+ruby script_mobile.rb 71 2 &
+sleep 5
+ruby script_mobile.rb 72 2 &
+sleep 5
+ruby script_mobile.rb 73 2 &
+sleep 5
+ruby script_mobile.rb 74 2 &
+sleep 5
+ruby script_mobile.rb 75 2 &
+sleep 5
+ruby script_mobile.rb 76 2 &
+sleep 5
+ruby script_mobile.rb 77 2 &
+sleep 5
+ruby script_mobile.rb 78 2 &
+sleep 5
+ruby script_mobile.rb 79 2 &
+sleep 5
+ruby script_mobile.rb 80 2 &
+sleep 5
+ruby script_mobile.rb 81 2 &
+sleep 5
+ruby script_mobile.rb 82 2 &
+sleep 5
+ruby script_mobile.rb 83 2 &
+sleep 5
+ruby script_mobile.rb 84 2 &
+sleep 5
+ruby script_mobile.rb 85 2 &
+sleep 5
+ruby script_mobile.rb 86 2 &
+sleep 5
+ruby script_mobile.rb 87 2 &
+sleep 5
+ruby script_mobile.rb 88 2 &
+sleep 5
+ruby script_mobile.rb 89 2 &
+sleep 5
+ruby script_mobile.rb 90 2 &
+sleep 5
+ruby script_mobile.rb 91 2 &
+sleep 5
+ruby script_mobile.rb 92 2 &
+sleep 5
+ruby script_mobile.rb 93 2 &
+sleep 5
+ruby script_mobile.rb 94 2 &
+sleep 5
+ruby script_mobile.rb 95 2 &
+sleep 5
+ruby script_mobile.rb 96 2 &
+sleep 5
+ruby script_mobile.rb 97 2 &
+sleep 5
+ruby script_mobile.rb 98 2 &
+sleep 5
+ruby script_mobile.rb 99 2 &
+sleep 5
